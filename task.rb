8010d44ca05e8584cb4b1500b5326b382dcdec2d
MatsuyamaTaqtsuya <tmatsu.inv@gmail.com>
2022-02-06 17:03:27 +0900

    Q15で三項演算子を利用するように修正

--- task.rb
+++ task.rb
@@ -153,17 +153,11 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  data1_result = data1.key?(:age) ? "OK" : "NG"
+  data2_result = data2.key?(:age) ? "OK" : "NG"
 
-  if data2.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  puts data1_result
+  puts data2_result
 
 end
 
